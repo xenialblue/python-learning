de7c681f0a82bb0e78b837f43e58aa4aad917fb9
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-01-24 13:25:12 +0700

    Update aritmatika.py

--- aritmatika.py
+++ aritmatika.py
@@ -1,2 +1,11 @@
-x = (2 + 3) * 10 - 3
+x = (2 + 3) * 10 - 3 / 2.4
 print(x)
+
+#angka sisa dari pembagian
+pembagian = 1039 % 5
+print(pembagian)
+
+#pangkat bilangan
+pertama = 5 ** 3
+kedua = 3 ** 8
+print(pertama, kedua)
