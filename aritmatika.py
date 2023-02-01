502e38105b56d4474ea232a47ea528504d303eba
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-02-01 09:51:55 +0700

    Update aritmatika.py

--- aritmatika.py
+++ aritmatika.py
@@ -15,5 +15,14 @@ huruf = "hello " + "world"
 print(huruf)
 
 #multiple string dalam satu perintah
-banyak = "hello world" * 100
+banyak = "hello world" * 10
 print(banyak)
+
+#penggabungan operator
+even_numbers = [2,4,6]
+odd_numbers = [1,3,5]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+#perkalian kolom
+print([1,2,3] *3)
