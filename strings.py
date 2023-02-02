1d4f13687215bf4ae78bc2722e0675ebefbf91f3
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-02-02 08:43:33 +0700

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,3 +1,16 @@
 testing = 'Sebuah kalimat untuk pengingat'
 kedua = testing[1:-1]
 print(kedua)
+
+#string format bisa pakai variabel
+x = "Nanda"
+print("My Name, %s!" % x)
+
+#dua argumen untuk string dalam variabel
+nama = "nanda"
+umur = 23
+print("%s berumur %d tahun. " % (nama, umur))
+
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s your current balance is $%s"
+print(format_string % data)
