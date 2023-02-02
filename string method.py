af28e4560ffb149891f27a20aef9383e5c5270cc
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-02-02 09:07:55 +0700

    Update string method.py

--- string method.py
+++ string method.py
@@ -8,3 +8,7 @@ print('nama' in testing)
 
 astring = "hello world!"
 print(astring[3:7:2])
+
+s = "Hey there! what shou"
+print("Length of s = %d" % len(s))
+print("The first occurence of the letter a = %d" % s.index("e"))
