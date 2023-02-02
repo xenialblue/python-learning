1c2cc7eda3cde062deeced90321adb7ce4089777
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-02-02 08:54:40 +0700

    push

--- string method.py
+++ string method.py
@@ -1,6 +1,10 @@
 testing = 'nama saya nanda'
 print(testing.upper())
+print(testing.lower())
 print(testing)
 print(testing.find('nanda'))
 print(testing.replace('n', 'p'))
 print('nama' in testing)
+
+astring = "hello world!"
+print(astring[3:7:2])
--- strings.py
+++ strings.py
@@ -14,3 +14,4 @@ print("%s berumur %d tahun. " % (nama, umur))
 data = ("John", "Doe", 53.44)
 format_string = "Hello %s %s your current balance is $%s"
 print(format_string % data)
+
