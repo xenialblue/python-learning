8420d7e75e0d8751f429f57cf997abf1219dfccf
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-02-06 10:59:29 +0700

    Update for-loops.py

--- for-loops.py
+++ for-loops.py
@@ -4,3 +4,16 @@ total = 0
 for price in prices:
     total += price
 print(f"Total: {total}")
+
+primes = [2,3,5,7]
+for prime in primes:
+    print(prime)
+
+for x in range(5):
+    print(x)
+    
+for x in range(3,6):
+    print(x)
+
+for x in range(3,8,2):
+    print(x)
