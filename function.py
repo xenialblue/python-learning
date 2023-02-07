f77c03016fa414acf8966f701026208681f5bcd9
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-02-07 10:19:44 +0700

    Create function.py

---
+++ function.py
@@ -0,0 +1,14 @@
+def my_function():
+    print("Function Here")
+    
+def argumen(username, greeting):
+    print("Hello %s and here is function %s"%(username, greeting))
+    
+def sum_twonumber(a,b):
+    return a+b
+
+my_function()
+argumen("John Doe", "a Great Day!")
+x = sum_twonumber(1,2)
+print(x)
+
