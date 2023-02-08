a2c5553cfdb0ae636e98ecff975a01048d122eb4
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-02-08 10:23:17 +0700

    Create class-and-object.py

---
+++ class-and-object.py
@@ -0,0 +1,13 @@
+class kelas:
+    variable = "tes"
+    
+    def function(self):
+        print("Pesan dari Kelas")
+        
+myobjectx = kelas()
+myobjecty = kelas()
+
+myobjecty.variable = "kitty"
+
+print(myobjectx.variable)
+print(myobjecty.variable)
