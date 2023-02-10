0be14eb49bd9869575194a7538f74720901f4676
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-02-10 10:32:16 +0700

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+height = [1.87,  1.87, 1.82, 1.91, 1.90, 1.85]
+weight_kg = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+
+np_weight_kg = np.array(weight_kg)
+np_weight_lbs = np_weight_kg * 2.2
+print(np_weight_lbs)
