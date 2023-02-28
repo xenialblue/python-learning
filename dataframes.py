86a350fb461cad845a7276401f5bb5dfcd3857c7
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-02-28 20:58:50 +0700

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -1,7 +1,8 @@
 dict = {"country": ["brazil","russia","indonesia","china","india","south africa"],
         "capital": ["brasilia","moscow","jakarta","beijing","new delhi","pretoria"],
         "area": [8.516, 23.668, 30.2019, 221.2000, 32.9872, 76.9008],
-        "populations": [300, 5090, 98723, 92384, 123745, 89732]}
+        "populations": [300, 5090, 98723, 92384, 123745, 89732],
+        "region": ["south america", "eastern europe", "southeast asia", "central asia", "west asia", "south africa"]}
 
 import pandas as pd
 brics = pd.DataFrame(dict)
