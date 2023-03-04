b3fb44740996189ce2398e7f93c22ec08175e62f
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-03-04 12:07:49 +0700

    Update variable-and-types.py

--- variable-and-types.py
+++ variable-and-types.py
@@ -1,32 +1,7 @@
-my_int = 10
-my_float = 10.0
-my_float2 = float(10)
-my_string = "10"
-
-#combination
-x = 20
-y = float(40)
-z = x + y
-print (z)
-
-a = "my "
-b = "name "
-c = "is blitz"
-d = a+b+c
-print (d)
-
-m, n = 10, 3
-print (m, n)
-
-#latihan
-ini_string = "latihan python"
-ini_integer = 20
-ini_float = float(50)
-
-#untuk statement pastikan pakai = 2 kali
-if ini_string == "latihan python":
-    print("String: %s" % ini_string)
-if isinstance(ini_integer, int) and ini_integer == 20:
-    print("Integer: %s" % ini_integer)
-if isinstance(ini_float, float) and ini_float == float(50):
-    print("Float: %s" % ini_float)
+nama = "nanda"
+umur = "23"
+number = int(umur)
+# comment
+print(f"Nama anda {nama} umur {umur}")
+print(isinstance(number, int))
+print(type(umur))
