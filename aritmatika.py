6c8be46bbd753946acadfad759c563df5494e812
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-03-06 13:24:20 +0700

    Update aritmatika.py

--- aritmatika.py
+++ aritmatika.py
@@ -1,28 +1,7 @@
-x = (2 + 3) * 10 - 3 / 2.4
-print(x)
-
-#angka sisa dari pembagian
-pembagian = 1039 % 5
-print(pembagian)
-
-#pangkat bilangan
-pertama = 5 ** 3
-kedua = 3 ** 8
-print(pertama, kedua)
-
-#operator untuk string
-huruf = "hello " + "world"
-print(huruf)
-
-#multiple string dalam satu perintah
-banyak = "hello world" * 10
-print(banyak)
-
-#penggabungan operator
-even_numbers = [2,4,6]
-odd_numbers = [1,3,5]
-all_numbers = odd_numbers + even_numbers
-print(all_numbers)
-
-#perkalian kolom
-print([1,2,3] *3)
+def adult(age):
+    if age > 18 :
+        print("True")
+        return True
+    else :
+        print("false")
+        return False
