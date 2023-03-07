58611f40c2408a00c90579c361f3e6122fde8ac5
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-03-07 14:28:43 +0700

    Update sets.py

---
+++ sets.py
@@ -0,0 +1,14 @@
+names = {
+    "Nanda",
+    "Adhit"
+}
+names2 = {
+    "Nanda"
+}
+
+inner = names & names2
+mod = names - names2
+print(inner)
+print(mod)
+
+#sets untuk index yang isinya tidak bisa sama dan jikalau ada kesamaan dalam isi index tersebut maka akan dianggap hanya satu isi index saja
