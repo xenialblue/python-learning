6860ef7f6d607bd4ee3cd160c6c757b8b1c476e5
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-03-07 16:04:11 +0700

    Update function.py

--- function.py
+++ function.py
@@ -1,25 +1,6 @@
-def my_function():
-    print("Function Here")
+def my_function(nama = "Halo ", umur = "Kawan"):
+    nama = input("Isi Nama Anda : ")
+    umur = input("Umur Anda : ")
+    print("Halo " + nama + " Umur kamu " + umur + " Tahun")
     
-def argumen(username, greeting):
-    print("Hello %s and here is function %s"%(username, greeting))
-    
-def sum_twonumber(a,b):
-    return a+b
-
 my_function()
-argumen("John Doe", "a Great Day!")
-x = sum_twonumber(1,2)
-print(x)
-
-def list_benefits():
-    return "Jago Coding", "Paaham matematika", "menjadi pintar", "sorting yang baik"
-def build_sentence(benefit):
-    return "%s is a benefit of function" %benefit
-
-def name_the_benefits_of_functions():
-    list_of_benefits = list_benefits()
-    for benefit in list_of_benefits:
-        print(build_sentence(benefit))
-        
-name_the_benefits_of_functions()
