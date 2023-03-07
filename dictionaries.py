3da17392ec6f0acc17d7d51e341c9748c6cafba7
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-03-07 14:22:12 +0700

    update

--- dictionaries.py
+++ dictionaries.py
@@ -1,17 +1,13 @@
 phonebook = {
-    "John" : 927808723,
-    "Jack" : 872130987,
-    "Jill" : 891723057,
-    "Jake" : 938273443
+    "Roger" : 92819,
+    "Bob" : 98219,
+    "Jill" : 91203
 }
-for name, number in phonebook.items():
-    print("Phone number of %s is %d" % (name, number))
-    
-#removing values
-phonebook.pop("Jill")
 
-if "Jake" in phonebook:
-    print("Jake is listed")
-    
-if "Jill" in phonebook:
-    print("Jill is not in list")
+phonebook["James"] = 92014
+
+del phonebook ["Bob"]
+print(phonebook.get("Jill"))
+print("Bob" in phonebook)
+print(list(phonebook.items()))
+print(len(phonebook))
