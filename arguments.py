f928b9f5a5db62896b2a64576cc24126ecaa0b93
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-03-08 14:53:17 +0700

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser(
     description="This Program is printing the name of my dogs"
 )
-parser.add_argument("-c", "--color", metavar="color", required=True, help="the color to search for")
+parser.add_argument("-c", "--color", metavar="color", required=True, help="the color to search for", choices={"red", "green"})
 
 args = parser.parse_args()
 print(args.color)
