ef4c9fbe4827c7a1885b7817d83bf56fe4f02ec9
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-03-08 13:26:35 +0700

    Update function.py

--- function.py
+++ function.py
@@ -4,3 +4,27 @@ def my_function(nama = "Halo ", umur = "Kawan"):
     print("Halo " + nama + " Umur kamu " + umur + " Tahun")
     
 my_function()
+
+#Nested Function
+def talk(phrase):
+    def say(word):
+        print(word)
+        
+    words = phrase.split(" ")
+    for word in words:
+        say(word)
+        
+talk("I'm going to buy a milk today")
+
+#Memanggil Variabel diluar Function Local menggunakan perintah NONLOCAL
+def count():
+    jumlah = 0
+    
+    def increment():
+        nonlocal jumlah
+        jumlah = jumlah + 1
+        print(jumlah)
+        
+    increment()
+    
+count()
