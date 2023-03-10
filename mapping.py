30a1b28dbb3000c6bf43e0c49f9c5739ca5a6dda
Nanda Nugraha <50394306+xenialblue@users.noreply.github.com>
2023-03-10 16:20:26 +0700

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -1,4 +1,10 @@
-numbers = [1,2,3]
+from functools import reduce
+
+numbers = [1,2,3,4,5,6,7,8,9]
+expenses = [
+    ("Dinner", 80),
+    ("Car Repair", 120)
+]
 
 def double(a):
     return a * 2
@@ -6,9 +12,11 @@ def double(a):
 def isEven(n):
     return n % 2 == 0
 
+sum = reduce(lambda a, b : a[1] + b[1], expenses)
+
 result = map(double, numbers)
 even = filter(isEven, numbers)
 
 print(list(result))
 print(list(even))
-
+print(sum)
